c90ea1e34834da5fc454020796270bc0791fee54
khethiwengema04-gif <khethiwe.ngema04@gmail.com>
2026-05-27 06:25:39 +0200

    Created a file with arithmetic operation

---
+++ Arithmetic_Operations.js
@@ -0,0 +1,17 @@
+ // Arithmetic Operations
+
+// Declare two variables
+let num1 = 40;
+let num2 = 5;
+
+// Store arithmetic operation results
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+// Print the results
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
