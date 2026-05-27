6e0d6f935731f0d68977013b18dbf76f8a156c03
khethiwengema04-gif <khethiwe.ngema04@gmail.com>
2026-05-27 06:27:09 +0200

    Created a file with variable reassignment

---
+++ Variable_Reassignment.js
@@ -0,0 +1,15 @@
+// Variable Reassignment
+
+//  Declare a variable 'favoriteColour'
+let favoriteColour = "Blue";
+
+//  Print the value of favoriteColour
+console.log("Favourite Colour:", favoriteColour);
+
+//  Reassign favoriteColour to a different colour
+favoriteColour = "Green";
+
+//  Print the new value of favoriteColour
+console.log("New Favourite Colour:", favoriteColour);
+
+
