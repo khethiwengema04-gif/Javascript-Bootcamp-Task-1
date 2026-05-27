8c22eac24ab5fdb0b8a30b059ace3193777cc3ee
khethiwengema04-gif <khethiwe.ngema04@gmail.com>
2026-05-27 06:26:22 +0200

    Created a file with viriables declared

---
+++ Variable_Declaration.js
@@ -0,0 +1,15 @@
+//Variable Declaration
+
+// Declare a variable 'name' and assign your name to it
+let name = "Khethiwe";
+
+//Declare a variable 'age' and assign your age to it
+let age = 22;
+
+//Declare a variable 'isStudent' and assign a boolean value
+let isStudent = true;
+
+//Print all three variables
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
