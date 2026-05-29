04ef02d719217d28131fad93fe4a0acad6ed0114
khethiwengema04-gif <khethiwe.ngema04@gmail.com>
2026-05-29 22:58:48 +0200

    Added other activities on this file

--- Variable_Declaration.js
+++ Variable_Declaration.js
@@ -1,4 +1,4 @@
-//Variable Declaration
+//Exercise1: Variable Declaration
 
 // Declare a variable 'name' and assign your name to it
 let name = "Khethiwe";
@@ -13,3 +13,36 @@ let isStudent = true;
 console.log("Name:", name);
 console.log("Age:", age);
 console.log("Is Student:", isStudent);
+
+//Exercise2: Variable Reassignment
+
+//  Declare a variable 'favoriteColour'
+let favoriteColour = "Blue";
+
+//  Print the value of favoriteColour
+console.log("Favourite Colour:", favoriteColour);
+
+//  Reassign favoriteColour to a different colour
+favoriteColour = "Green";
+
+//  Print the new value of favoriteColour
+console.log("New Favourite Colour:", favoriteColour);
+
+ //Exercise3: Arithmetic Operations
+
+// Declare two variables
+let num1 = 40;
+let num2 = 5;
+
+// Store arithmetic operation results
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+// Print the results
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+
